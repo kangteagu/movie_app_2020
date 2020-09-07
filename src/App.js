8dd5fcc1c691bac25a746e1ec673336555543237
kangteagu <67666293+kangteagu@users.noreply.github.com>
2020-09-07 17:20:15 +0900

    Movie_app_2020
    완성판

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import  About from "./routes/About";
+import "./App.css";
+import Navigation from "./components/Navigation";
+
 
 function App(){
   return(
   <HashRouter>
+    <Navigation/>
     <Route path="/" exact={true} component={Home} />
     <Route path="/about" component={About} />
   </HashRouter>
   );
 }
 
-export default App;
+export default App;      
