01b3b40039bbf052aab43bfb27c1961e0a347886
kangteagu <67666293+kangteagu@users.noreply.github.com>
2020-09-07 15:05:13 +0900

    movie__app (new)
    업데이트

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from "axios";
 import Movie from "./Movie";
+import "./App.css";
 
 
 class App extends React.Component{
@@ -20,22 +21,32 @@ class App extends React.Component{
     this.getMovies();
   }
 
-  render(){
+  render() {
     const { isLoading, movies } = this.state;
-    return <div>{isLoading ? "Loading..." : movies.map(movie =>{
-      console.log(movie);
-      return (
-      <Movie 
-        id={movie.id}
-        year={movie.year} 
-        title={movie.title} 
-        summary={movie.summary} 
-        poster={movie.medium_cover_image}
-      />
-      );
-    })}</div>;
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="Loader">
+            <span className="loader__text">Lodaing...</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie =>(
+              <Movie
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.title}
+                summary={movie.summary}
+                poster={movie.medium_cover_image}
+                genres={movie.genres}
+              />
+           ))}
+          </div>
+        )}
+      </section>
+    );
   }
 }
- 
 
 export default App;
